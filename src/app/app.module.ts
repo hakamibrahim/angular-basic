a5f40ba9583ec9a59dcde6e5f02fdf0206ab3167
h4km <hakamibrahim7@gmail.com>
2018-04-09 21:51:10 +0700

    module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FriendComponent } from './friend/friend.component';
+import { FriendAddComponent } from './friend/friend-add/friend-add.component';
+import { FriendListComponent } from './friend/friend-list/friend-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendComponent,
+    FriendAddComponent,
+    FriendListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
